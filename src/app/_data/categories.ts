6cad2710df621f1d72ad1a12ffbffd680da7b03f
Hoang Doan An <an.hoang@asnet.com.vn>
2025-07-07 10:50:01 +0700

    refactor: update category image paths to use API_URL for consistency

--- src/app/_data/categories.ts
+++ src/app/_data/categories.ts
@@ -2,9 +2,9 @@ import { Category } from './types';
 
 export const categories: Category[] = [
   { id: 'phone', name: 'Phone', image: `${process.env.API_URL}/images/category-phone.svg` },
-  { id: 'computer', name: 'Computer', image: '/images/category-computer.svg' },
+  { id: 'computer', name: 'Computer', image: `${process.env.API_URL}/images/category-computer.svg` },
   { id: 'smartwatch', name: 'Smart Watch', image: `${process.env.API_URL}/images/category-smartwatch.svg` },
-  { id: 'camera', name: 'Camera', image: '/images/category-camera.svg' },
+  { id: 'camera', name: 'Camera', image: `${process.env.API_URL}/images/category-camera.svg` },
   { id: 'headphone', name: 'Headphone', image: `${process.env.API_URL}/images/category-headphone.svg` },
   { id: 'gaming', name: 'Gaming', image: `${process.env.API_URL}/images/category-gaming.svg` },
   { id: 'vision', name: 'Vision', image: `${process.env.API_URL}/images/category-vision.svg` },
